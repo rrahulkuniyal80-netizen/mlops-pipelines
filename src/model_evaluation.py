0a77b51fd019070998b6d2b72fc6c50ea26ffee5
rrahulkuniyal80-netizen <rrahulkuniyal80@gmail.com>
2026-07-17 12:33:13 +0530

    new functionality added

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -6,7 +6,7 @@ import json
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 import logging
 import yaml
-#from dvclive import Live
+from dvclive import Live
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
@@ -30,7 +30,7 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-"""def load_params(params_path: str) -> dict:
+def load_params(params_path: str) -> dict:
     
     try:
         with open(params_path, 'r') as file:
@@ -45,7 +45,7 @@ logger.addHandler(file_handler)
         raise
     except Exception as e:
         logger.error('Unexpected error: %s', e)
-        raise """
+        raise 
 
 def load_model(file_path: str):
     """Load the trained model from a file."""
@@ -112,8 +112,7 @@ def save_metrics(metrics: dict, file_path: str) -> None:
 
 def main():
     try:
-        #params = load_param
-        # s(params_path='params.yaml')
+        params = load_params(params_path='params.yaml')
         clf = load_model('./models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
         
@@ -123,12 +122,12 @@ def main():
         metrics = evaluate_model(clf, X_test, y_test)
 
         # Experiment tracking using dvclive
-        """with Live(save_dvc_exp=True) as live:
+        with Live(save_dvc_exp=True) as live:
             live.log_metric('accuracy', accuracy_score(y_test, y_test))
             live.log_metric('precision', precision_score(y_test, y_test))
             live.log_metric('recall', recall_score(y_test, y_test))
 
-            live.log_params(params)"""
+            live.log_params(params)
         
         save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
